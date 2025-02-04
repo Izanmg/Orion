4ca659c6584ecf55a8b0c06a30610547a47605d0
Izan <izanmg2706@gmail.com>
2025-02-04 12:27:18 +0100

    Añadido el boton de vuelta atras en el paso 2 de registro

--- orion_web/users/static/script.js
+++ orion_web/users/static/script.js
@@ -27,4 +27,33 @@ document.addEventListener("DOMContentLoaded", function () {
         btnLogin.classList.remove("hidden");
         btnRegister.classList.add("hidden");
     });
+
+    // Añadir la funcionalidad de mostrar/ocultar campos del formulario de registro
+    const nextStepButton = document.getElementById("next-step");
+    const stepBackButton = document.getElementById("step-back");
+    const firstStep = document.getElementById("first-step");
+    const secondStep = document.getElementById("second-step");
+
+    nextStepButton.addEventListener("click", function () {
+        // Ocultar el primer paso (Nombre, Apellidos, Nombre de usuario)
+        firstStep.classList.add("hidden");
+        
+        // Mostrar el segundo paso (Correo electrónico, Contraseña, Confirmar Contraseña)
+        secondStep.classList.remove("hidden");
+
+        stepBackButton.classList.remove("hidden");
+    });
+
+    stepBackButton.addEventListener("click", function () {
+        // Mostrar el primer paso
+        firstStep.classList.remove("hidden");
+        
+        // Ocultar el segundo paso
+        secondStep.classList.add("hidden");
+    
+        // Ocultar el botón de "Atrás" porque estamos en el primer paso
+        stepBackButton.classList.add("hidden");
+    });
+    
+    
 });
