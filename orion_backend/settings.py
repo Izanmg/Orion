0f5503a33344b7a9e411ae68a88e7c7b1235dc14
Izan Mendoza <izanmg2706@gmail.com>
2025-03-01 19:43:10 +0100

    Funcion de añadir/cambiar foto de perfil hecha, programa terminado

--- orion_backend/settings.py
+++ orion_backend/settings.py
@@ -144,3 +144,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'users.CustomUser'
 
 LOGOUT_REDIRECT_URL = reverse_lazy('')
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR,"media")
